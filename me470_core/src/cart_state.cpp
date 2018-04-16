e4c7b78fe0aa59af5ed26c18ded9d6d15bd77edc
Zixu <zzhng122@illinois.edu>
2018-04-16 17:03:23 -0500

    change core

--- me470_core/src/cart_state.cpp
+++ me470_core/src/cart_state.cpp
@@ -16,8 +16,10 @@ void cart_state::set_init(bool input)
 	if(input)
 	{
 		cout<<"Cart Initialization Done"<<endl;
+		system_status=READY_FOR_DEL;
 		this->reset_deliverd();
-	}
+	}else
+		system_status=WAIT_FOR_INIT;
 }
 
 bool cart_state::is_deliver_start()
@@ -34,24 +36,57 @@ void cart_state::set_deliver_start(bool input)
 {
 	if(input){
 		this->deliver_start=true;
+		this->delivered=false;
 		this->ready_for_deliver=false;
+		system_status=DELIVERING；
 	}
 }
 
 void cart_state::set_delivered(bool input)
 {
 	this->delivered=input;
+	system_status=DELIVERED;
 }
 
-void cart_state::reset_deliverd()
+void cart_state::reset_delivery()
 {
 	this->deliver_start=false;
 	this->delivered=false;
 	this->ready_for_deliver=true;
+	system_status=READY_FOR_DEL;
 }
 
 void cart_state::set_loading(bool input)
 {
-	if(input)
+	if(input){
 		this->ready_for_deliver=false;
+		system_status=LOADING;
+	}
+
+}
+
+void cart_state::failure(bool input)
+{
+	if(input)
+	{
+		this->ready_for_deliver=false;
+		system_status=FAILED;
+	}
+}
+
+bool cart_state::is_failed()
+{
+	if(system_status==FAILED)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+int cart_state::Get_current_status()
+{
+	return system_status;
 }
--- me470_core/src/me470_ros_node.cpp
+++ me470_core/src/me470_ros_node.cpp
@@ -15,56 +15,22 @@
 
 
 using namespace std;
+
 /* 
 Global Variable
 */
-enum Status{
-	DISCONNECTED=0,
-	WAIT_FOR_INIT=1,
-	LOADING=2,
-	READY_FOR_DEL=3,
-	DELIVERING=4,
-	DELIVERED=5,
-	FAILED=6
-};
-
-
-Status System_status=DISCONNECTED;
 ImageConverter * ic_ptr;
-//april_detector Detector;
 cart_state Cart;
 
 /*
 Following are publisher
 */
-ros::Publisher status_pub;
 ros::Publisher linear_servo_pub;
-ros::Publisher error_pub;
-ros::Publisher deliverd_pub;
+ros::Publisher init_request_pub;
 
 /*
 Following are subscriber function
 */
-
-// subscriber for /cart_msg/init_done to determine if initializaion is finished
-
-void status_publisher(const sensor_msgs::ImageConstPtr& msg) //if camera works
-{
-	std_msgs::UInt16 status2pub;
-	status2pub.data=System_status;
-	status_pub.publish(status2pub);  	
-}
-
-void init_status_sub(const std_msgs::Bool& msg)
-{
-	System_status=WAIT_FOR_INIT;
-	if(~Cart.is_init())
-	{
-		Cart.set_init(msg.data);
-		if(Cart.is_init())//if receve init done msg from cart
-			System_status=READY_FOR_DEL;
-	}
-}
 
 void deliver_status_sub(const std_msgs::UInt16 & msg)
 {
@@ -77,46 +43,54 @@ void deliver_status_sub(const std_msgs::UInt16 & msg)
 	DELIVERED=5,
 	FAILED=6
 	*/
-	if(Cart.is_init())
+	switch(msg.data)
 	{
-		switch(msg.data)
-		{
-			case 2: //robot is loading
-				Cart.set_loading(true);
-				System_status=LOADING;
-				break;
+		case 0: // fresh start
+			Cart.set_init(false);
+			std_msgs::Bool msg2pub;
+			msg2pub.data=1;
+			init_request_pub.publish(msg2pub);
+			break;
 
-			case 3: //ready for delivery after loading
-				if(!Cart.is_cart_ready())
-					Cart.reset_deliverd();
-				System_status=READY_FOR_DEL;
-				break;
+		case 1: // robot has not been initalized
+			std_msgs::Bool msg2pub;
+			msg2pub.data=1;
+			init_request_pub.publish(msg2pub);
+			break;
 
-			case 4: //robot received the deliver message
+		case 2: //robot is loading	
+			Cart.set_loading(true);
+			break;
+
+		case 3: //ready for delivery after loading
+			if(!Cart.is_init())
+				Cart.set_init(true);
+			else if(!Cart.is_cart_ready())
+				Cart.reset_deliverd();
+			break;
+
+		case 4: //robot received the deliver message
+			if(!Cart.is_deliver_start()){
 				Cart.set_deliver_start(true);
-				System_status=DELIVERING;
-				break;
+			}
+			break;
 
-			case 5: //robot finished the delivery
+		case 5: //robot finished the delivery
+			if(!Cart.is_delivered()){
 				Cart.set_delivered(true);
-				System_status=DELIVERED;
-				std_msgs::Bool deliverd_msg;
-				deliverd_msg.data=true;
-				deliverd_pub.publish(deliverd_msg);
-				break;
+			}	
+			break;
 
-			case 6: //deliver failure
-				System_status=FAILED;
-				std_msgs::Bool error_msg;
-				error_msg.data=true;
-				error_pub.publish(error_msg);
-				break;
+		case 6: //deliver failure
+			if(!Cart.is_failed()){
+				Cart.failure(true);
+			}
+			break;
 
-			default:
-				System_status=READY_FOR_DEL;
-				break;
-		}
+		default:
+			break;
 	}
+	
 }
 
 void waypoint_reached_sub(const std_msgs::Bool& msg)
@@ -125,7 +99,7 @@ void waypoint_reached_sub(const std_msgs::Bool& msg)
 	{
 		if(msg.data)//if the waypoint has been arrived
 		{
-			float linear_distance = 100.0*(ic_ptr->Detector).detection_distance(ic_ptr->gray,0);
+			float linear_distance = 10.0*(ic_ptr->Detector).detection_distance(ic_ptr->gray,0);
 			if(linear_distance!=-1){//target tag detected
 				std_msgs::UInt16 linear2pub;
 				linear2pub.data=(int) linear_distance;
@@ -142,13 +116,11 @@ int main(int argc, char** argv)
   ImageConverter ic;
   ic_ptr=&ic;
 
-  status_pub = n.advertise<std_msgs::UInt16>("/cart_bot_msg/status_pub", 1);
+  
   linear_servo_pub = n.advertise<std_msgs::UInt16>("/cart_msg/deliver_request", 1);
-  error_pub = n.advertise<std_msgs::Bool>("/cart_bot_msg/error_msg",1);
-  deliverd_pub = n.advertise<std_msgs::Bool>("/cart_bot_msg/deliverd_success",1)
+  init_request_pub = n.advertise<std_msgs::Bool>("/cart_msg/init_request",1);
 
-  ros::Subscriber sub0 = n.subscribe("/usb_cam/image_raw",1,status_publisher);
-  ros::Subscriber sub1 = n.subscribe("/cart_msg/init_done",1,init_status_sub);
+  ros::Subscriber sub1 = n.subscribe("/cart_msg/cart_status",1,deliver_status_sub);
   ros::Subscriber sub2 = n.subscribe("/cart_bot_msg/waypoint_reached",1,waypoint_reached_sub);
 
   
